82734ec750daf01da94be959bbe4f1a68354adbb
Neil Muller <drnlmuller@gmail.com>
2023-03-20 14:16:58 +0200

    Fill in more commands

--- am5dieroller/bot/main.py
+++ am5dieroller/bot/main.py
@@ -10,7 +10,7 @@ from discord import app_commands
 
 from am5dieroller.ars_magica_rolls.stressed import stressed_roll
 from am5dieroller.ars_magica_rolls.formulaic import formulaic_roll
-from am5dieroller.ars_magica_rolls.spont_rolls import spont_roll, fatiguing_spont_roll
+from am5dieroller.ars_magica_rolls.spont_rolls import spont_non_roll, fatiguing_spont_roll
 from am5dieroller.ars_magica_rolls.simple import simple_roll
 #from am5dieroller.ars_magica_rolls.botch import botch_roll
 
@@ -75,11 +75,21 @@ client = DieRollerClient(guilds)
 async def stressed(interaction: discord.Interaction, modifier: Optional[int] = 0):
     rolls, total, outcome = stressed_roll(modifier)
     if len(rolls) > 1:
-        result = f'Rolls: {rolls}. Total (with modifier {modifier}: {total}'
+        result = f'Rolls: {rolls}. Total (with modifier {modifier}: **{total}**'
     else:
-        result = f'Roll: {rolls[0]}. Total (with modifier {modifier}: {total}'
+        result = f'Roll: {rolls[0]}. Total (with modifier {modifier}: **{total}**'
     if outcome:
-        result += f'\n{outcome}'
+        result += f'\n**{outcome}**\n'
+    await interaction.response.send_message(result)
+
+
+@client.tree.command()
+@app_commands.describe(
+    modifier="modifier to add to the roll (optional)",
+)
+async def simple(interaction: discord.Interaction, modifier: Optional[int] = 0):
+    rolls, total, outcome = simple_roll(modifier)
+    result = f'Roll: {rolls[0]}. Total (with modifier {modifier}: **{total}**'
     await interaction.response.send_message(result)
 
 
@@ -89,7 +99,13 @@ async def stressed(interaction: discord.Interaction, modifier: Optional[int] = 0
     target="The target level of the spell",
 )
 async def formulaic(interaction: discord.Interaction, casting_score: int, target: int):
-     await interaction.response.send_message(f"Called with {casting_score} and {target}")
+    rolls, total, outcome = formulaic_roll(casting_score, target)
+    if len(rolls) > 1:
+        result = f'Rolls: {rolls}. Total (with casting score {casting_score}: **{total}** (against {target})'
+    else:
+        result = f'Roll: {rolls[0]}. Total (with casting score {casting_score}: **{total}** (against {target})'
+    result += f'\n**{outcome}**\n'
+    await interaction.response.send_message(result)
 
 @client.tree.command()
 @app_commands.describe(
@@ -97,7 +113,10 @@ async def formulaic(interaction: discord.Interaction, casting_score: int, target
     target="The target level of the spell",
 )
 async def spontaneous(interaction: discord.Interaction, casting_score: int, target: int):
-     await interaction.response.send_message(f"Called with {casting_score} and {target}")
+    _, total, modified_total, outcome = spont_non_roll(casting_score, target)
+    result = f'Total: {total}. Final total **{modified_total}** (against {target})'
+    result += f'\n**{outcome}**\n'
+    await interaction.response.send_message(result)
 
 
 @client.tree.command()
@@ -106,15 +125,18 @@ async def spontaneous(interaction: discord.Interaction, casting_score: int, targ
     target="The target level of the spell",
 )
 async def fspont(interaction: discord.Interaction, casting_score: int, target: int):
-     await interaction.response.send_message(f"Called with {casting_score} and {target}")
-
+    rolls, total, modified_total, outcome = fatiguing_spont_roll(casting_score, target)
+    if len(rolls) > 1:
+        result = f'Rolls: {rolls}. Total (with casting score {casting_score}: {total}. Final total **{modified_total}** (against {target})'
+    else:
+        result = f'Roll: {rolls[0]}. Total (with casting score {casting_score}: {total}. Final total **{modified_total}** (against {target})'
+    result += f'\n**{outcome}**\n'
+    await interaction.response.send_message(result)
 
 
 @client.tree.command()
 async def about(interaction: discord.Interaction):
-     await interaction.response.send_message(ABOUT)
-
-
+    await interaction.response.send_message(ABOUT)
 
 
 client.run(token)
