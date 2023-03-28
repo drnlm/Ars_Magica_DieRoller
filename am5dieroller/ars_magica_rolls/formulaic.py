487aa70f3cd19988a1503f9065431c92b51f2f46
Neil Muller <drnlmuller@gmail.com>
2023-03-28 23:05:47 +0200

    Add ritual spell

--- am5dieroller/ars_magica_rolls/formulaic.py
+++ am5dieroller/ars_magica_rolls/formulaic.py
@@ -32,7 +32,7 @@ def formulaic_roll(modifier, target):
 def formulaic_simple_roll(modifier, target):
     """Cast a formulaic spell using a simple die.
 
-       Return the result (success, success with fatigure, failure)"""
+       Return the result (success, success with fatigue, failure)"""
     roll = die_roller()
     result = roll + modifier
     if result >= target:
@@ -42,3 +42,32 @@ def formulaic_simple_roll(modifier, target):
     else:
         outcome = "failure (with fatigue)"
     return [roll], result, outcome
+
+
+def ritual(modifier, target):
+    """Cast a ritual spell using a stressed die.
+
+       Return the result (success with fatigue (varying levels), failure)
+       and possible botches"""
+    rolls, total = stressed_die()
+    result = total + modifier
+    if result >= target - 10:
+        if rolls[0] == 0:
+            outcome = "possible success"
+        else:
+            outcome = "success"
+    else:
+        outcome = "failure"
+    if result >= target:
+        outcome += " (1 fatigue level)"
+    elif result >= target - 5:
+        outcome += " (2 fatigue levels)"
+    elif result >= target - 10:
+        outcome += " (3 fatigue levels)"
+    elif result >= target - 15:
+        outcome += " (4 fatigue levels)"
+    else:
+        outcome += " (5 fatigue levels)"
+    if rolls[0] == 0:
+        outcome += ' (possible botch)'
+    return rolls, result, outcome
